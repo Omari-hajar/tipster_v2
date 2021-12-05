e2095f26c7da391b9b0ab1da513bf671327e4f9d
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-12-05 09:26:25 +0300

    Initial Commit

---
+++ tipster_v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipster_v2
+//
+//  Created by Hajar Alomari on 05/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tipster_v2UITests/tipster_v2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tipster_v2UITestsLaunchTests.swift
+//  tipster_v2UITests
+//
+//  Created by Hajar Alomari on 05/12/2021.
+//
+
+import XCTest
+
+class tipster_v2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
